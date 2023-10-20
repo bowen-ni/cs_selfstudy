c31860e0144c7d037c3148742f286d63e309431f
Bowen Ni <nibowentony@gmail.com>
2023-10-20 13:17:01 -0400

    first commit

---
+++ recursion/find_min.py
@@ -0,0 +1,21 @@
+#first method
+def find_min(my_list):
+  if not my_list:
+    return None
+  if len(my_list) == 1:
+    return my_list[0]
+  for i in range(len(my_list)):
+    if my_list[i] <= find_min(my_list[(i+1):]):
+      return my_list[i]
+
+
+#second method
+# def find_min(my_list, min = None):
+#   if not my_list:
+#     return min
+#
+#   if not min or my_list[0] < min:
+#     min = my_list[0]
+#   return find_min(my_list[1:], min)
+
+print(find_min([13, 72, 19, 5, 86]))
