3e776211c56172ce70a602bc2e0c6f341d1782ef
bowen-ni <nibowentony@gmail.com>
2023-10-20 13:29:32 -0400

    Update find_min.py

--- recursion/find_min.py
+++ recursion/find_min.py
@@ -17,5 +17,5 @@ def find_min(my_list):
 #   if not min or my_list[0] < min:
 #     min = my_list[0]
 #   return find_min(my_list[1:], min)
-
-print(find_min([13, 72, 19, 5, 86]))
+#
+# print(find_min([13, 72, 19, 5, 86]))
